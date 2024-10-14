74db305ae2b841672b0d04ca1ab3b50090c21181
KASRA10 <kasrasparrow@gmail.comm>
2024-10-14 23:36:40 +0330

    add Toast And FOnt Asset as well as design AppBar

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 
 void main() {
   runApp(
@@ -18,8 +21,52 @@ class GeneratingWords extends StatefulWidget {
 }
 
 class _GeneratingWordsState extends State<GeneratingWords> {
+  // ToastMessage
+  void showToastMessage(String message) => Fluttertoast.showToast(
+        msg: message,
+        backgroundColor: Colors.pink,
+        textColor: Colors.white,
+        fontSize: 21,
+        gravity: ToastGravity.CENTER,
+        toastLength: Toast.LENGTH_LONG,
+      );
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        backgroundColor: Colors.pink,
+        centerTitle: true,
+        leading: IconButton(
+          onPressed: () => exit(0),
+          icon: const Icon(
+            Icons.close,
+            color: Colors.white,
+            size: 28,
+            semanticLabel: 'Close Icon For Exiting From Application',
+          ),
+        ),
+        title: const Text(
+          'Generate Words!',
+          style: TextStyle(
+            fontFamily: 'Jua',
+            color: Colors.white,
+          ),
+        ),
+        actions: [
+          MenuItemButton(
+            onPressed: () => showToastMessage(
+              'Not Available!',
+            ), // End Of showToastMessage()
+            child: const Icon(
+              Icons.menu,
+              color: Colors.white,
+              semanticLabel: 'Menu Icon, Access To Main Setting Of App',
+            ),
+          ),
+        ], // End Of Actions
+      ),
+    );
   }
 }
